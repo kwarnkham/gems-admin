e3b3198ceb94e22da455bf32a0e452d8ad3d3173
Moon <kwarnkham.moon@gmail.com>
2024-01-29 10:40:33 +0630

    #

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,4 +1,6 @@
 import enUS from './en-US'
+import mm from './mm'
+import zhCN from './zh-CN'
 
 export default {
   'en-US': enUS
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -16,7 +16,7 @@
           <span @click="$router.push({ name: 'home' })">Gems Admin</span>
         </q-toolbar-title>
 
-        <!-- <div>Quasar v{{ $q.version }}</div> -->
+        <q-btn icon="img:icons/us.png" flat @click="chooseLanguage" />
       </q-toolbar>
     </q-header>
 
@@ -30,4 +30,43 @@
   </q-layout>
 </template>
 
-<script setup></script>
+<script setup>
+import { useQuasar } from "quasar";
+import { watch } from "vue";
+
+const { dialog, lang } = useQuasar();
+const appLanguages = languages.filter((lang) =>
+  ["en-US", "zh-CN", "mm"].includes(lang.isoName)
+);
+const langOptions = appLanguages.map((lang) => ({
+  label: lang.nativeName,
+  value: lang.isoName,
+}));
+
+watch(language, (val) => {
+  import(
+    /* webpackInclude: /(mm|en-US|zh-CN)\.js$/ */
+    "quasar/lang/" + val
+  ).then((language) => {
+    lang.set(language.default);
+  });
+});
+
+const language = ref(lang.isoName);
+const chooseLanguage = () => {
+  dialog({
+    noBackdropDismiss: true,
+    options: {
+      type: "radio",
+      model: "en-US",
+      items: [
+        { label: "English", value: "en-US" },
+        { label: "中文", value: "zh-CN" },
+        { label: "မြန်မာ", value: "mm" },
+      ],
+    },
+  }).onOk((val) => {
+    console.log(val);
+  });
+};
+</script>
