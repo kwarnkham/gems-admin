a747e64dfe74da639c009f03c0b24122e108ab99
Moon <kwarnkham.moon@gmail.com>
2024-01-29 12:10:27 +0630

    cilent translate

--- src/layouts/ClientLayout.vue
+++ src/layouts/ClientLayout.vue
@@ -5,25 +5,39 @@
         active-color="white"
         active-bg-color="black"
         indicator-color="white"
+        class="row justify-evenly no-wrap"
       >
         <q-route-tab
+          class="col-3"
           :to="{ name: 'client-home' }"
           replace
           icon="home"
           no-caps
         />
         <q-route-tab
+          class="col-3"
           :to="{ name: 'client-item-list' }"
           replace
           icon="img:/diamond.png"
           no-caps
         />
         <q-route-tab
+          class="col-3"
           :to="{ name: 'contact' }"
           replace
           icon="support_agent"
           no-caps
         />
+        <q-btn
+          stretch
+          icon="translate"
+          @click.stop="chooseLanguage"
+          name="lang"
+          flat
+          class="col-3"
+          size="md"
+        >
+        </q-btn>
       </q-tabs>
     </q-footer>
 
@@ -37,7 +51,25 @@
   </q-layout>
 </template>
 
-<script setup></script>
+<script setup>
+import { useQuasar } from "quasar";
+import useLocale from "src/composables/locale";
+
+const { dialog } = useQuasar();
+const { langOptions, language } = useLocale();
+const chooseLanguage = () => {
+  dialog({
+    noBackdropDismiss: true,
+    options: {
+      type: "radio",
+      model: language.value,
+      items: langOptions,
+    },
+  }).onOk((val) => {
+    language.value = val;
+  });
+};
+</script>
 
 <style scoped lang="scss">
 .page {
